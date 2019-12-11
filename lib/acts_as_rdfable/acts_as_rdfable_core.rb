08763549d343bd0ca17833c13e968201d263ca62
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2019-12-11 09:27:05 -0700

    Change table_name to be call as a class method
    There is a small bug that stops the gem from working as intended when
    adding the class predicates.

--- lib/acts_as_rdfable/acts_as_rdfable_core.rb
+++ lib/acts_as_rdfable/acts_as_rdfable_core.rb
@@ -6,11 +6,11 @@ module ActsAsRdfable::ActsAsRdfableCore
       raise InvalidClassError unless self < ActiveRecord::Base
 
       define_method :rdf_annotations do
-        RdfAnnotation.for_table(self.table_name)
+        RdfAnnotation.for_table(self.class.table_name)
       end
 
       define_method :rdf_annotation_for_attr do |attr|
-        RdfAnnotation.for_table_column(self.table_name, attr)
+        RdfAnnotation.for_table_column(self.class.table_name, attr)
       end
     end
   end
