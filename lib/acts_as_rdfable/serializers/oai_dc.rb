c186ff1ba14bb362ee5a9fc7cdef74bc3f146a97
Matt Barnett <matt@sixtyodd.com>
2020-02-07 16:25:56 -0700

    better align OAI:DC required fields with LAC

--- lib/acts_as_rdfable/serializers/oai_dc.rb
+++ lib/acts_as_rdfable/serializers/oai_dc.rb
@@ -6,13 +6,14 @@ class OaiDc
              'xmlns:dc':'http://purl.org/dc/elements/1.1/',
              'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
              'xsi:schemaLocation': 'http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd') do |oai|
-      oai.dc(:title, object.title) if object.title.present?
-      object.creator.each {|c| oai.dc :creator, c} if object.creator.present?
+
+      oai.dc(:title, object.title)
+      object.creator.each {|c| oai.dc :creator, c}
       object.contributor.each {|c| oai.dc :contributor, c} if object.contributor.present?
       oai.dc(:rights, object.rights) if object.rights.present?
-      oai.dc(:date, object.date) if object.date.present?
+      oai.dc(:date, object.date)
       oai.dc(:publisher, object.publisher) if object.publisher.present?
-      object.identifiers.each {|i| oai.dc :identifier, i} if object.identifiers.present?
+      object.identifiers.each {|i| oai.dc :identifier, i}
       object.subject.each {|s| oai.dc :subject, s} if object.subject.present?
       oai.dc(:description, object.description) if object.description.present?
       oai.dc(:type, object.type) if object.type.present?
