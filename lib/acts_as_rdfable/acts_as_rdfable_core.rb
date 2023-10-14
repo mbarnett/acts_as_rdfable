e6f8604ccc2c9c3dcf488a5f887adf5b374abf74
murny <1930474+murny@users.noreply.github.com>
2023-10-14 16:43:07 -0600

    Use `.class` instead of `.singleton_class` due to issue with Rails 7

--- lib/acts_as_rdfable/acts_as_rdfable_core.rb
+++ lib/acts_as_rdfable/acts_as_rdfable_core.rb
@@ -10,20 +10,20 @@ module ActsAsRdfable::ActsAsRdfableCore
       formats.each { |format| ActsAsRdfable::Serializer.register_format_for_class(self, format: format) }
 
       define_method :rdf_annotations do
-        self.singleton_class.rdf_annotations
+        self.class.rdf_annotations
       end
 
       define_method :rdf_annotation_for_attr do |attr|
-        self.singleton_class.rdf_annotation_for_attr attr
+        self.class.rdf_annotation_for_attr attr
       end
 
       define_method :attribute_for_rdf_annotation do |annotation|
-        self.singleton_class.attribute_for_rdf_annotation annotation
+        self.class.attribute_for_rdf_annotation annotation
       end
 
       define_method :serialize_metadata do |format:, into_document:|
         format = format.to_sym unless format.is_a?(Symbol)
-        raise InvalidArgumentError unless self.singleton_class.supported_metadata_serialization_formats.include?(format)
+        raise InvalidArgumentError unless self.class.supported_metadata_serialization_formats.include?(format)
         ActsAsRdfable::Serializer.serialize(instance: self, format: format, xml_doc: into_document)
       end
 
